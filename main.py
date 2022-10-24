5e76d93b244c3001b978b1823d3552744be16e39
Дмитрий <103871402+brokerr1@users.noreply.github.com>
2022-10-25 00:33:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from scripts import check
 from random import choice as ch
 
 
-bot = Bot(token='5750066702:AAGYdNJBGumvmuYaW8_eWh8gy1yv3uYAZZ4')
-updater = Updater(token='5750066702:AAGYdNJBGumvmuYaW8_eWh8gy1yv3uYAZZ4')
+bot = Bot(token='')
+updater = Updater(token='')
 dispatcher = updater.dispatcher
 
 data = {2: 4, 3: 4, 4: 4, 5: 4, 6: 4, 7: 4, 8: 4, 9: 4, 10: 4, 'Валет': 4, 'Дама': 4, 'Король': 4,
